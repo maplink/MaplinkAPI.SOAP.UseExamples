6174d3452cdd0516e722d1ed68ee1b468ca730f8
Eduardo Costa <eduardo.costa@maplink.com.br>
2013-04-22 13:06:31 -0300

    Initial files

---
+++ MaplinkAPI.SOAP.UseExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaplinkAPI.SOAP.UseExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            var addressFinder = new AddressFinder();
+            addressFinder.FindAddress();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MaplinkAPI.SOAP.UseExamples/Token.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace MaplinkAPI.SOAP.UseExamples
+{
+    public class Token
+    {
+        private string _token;
+
+        public string Value
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_token))
+                {
+                    var tokenNotFound = "TokenNotFound";
+                    _token = ConfigurationManager.AppSettings["token"] ?? tokenNotFound;
+                }
+
+                return _token;
+            }
+        }
+    }
+}
